07131bf2bde47d2225c81c8a6b2e68744243a1a0
Halik1234 <145970118+Halik1234@users.noreply.github.com>
2023-10-28 11:09:56 +0700

    Add files via upload

---
+++ Tugas1.py
@@ -0,0 +1,26 @@
+a = "Kode Kendaraan :"
+b = "Nama Kendaraan :"
+c = "Merk Kendaraan :"
+d = "Jenis Kendaraan :"
+e = "CC Kendaraan :"
+f = "Warna Kendaraan :"
+g = "Harga :"
+h = "Jumlah Ban :"
+
+
+
+kendaraan = ["B 1456 PAC","vixion",220,"hitam"]
+
+kendaraan.append("30.000.000.00")
+kendaraan.append("2")
+kendaraan.insert(2, "Yamaha")
+kendaraan.insert(3, "sport")
+
+print(a,kendaraan[0])
+print(b,kendaraan[1])
+print(c, kendaraan[2])
+print(d, kendaraan[3])
+print(e, kendaraan[4])
+print(f, kendaraan[5])
+print(g, kendaraan[6])
+print(h, kendaraan[7])
---
+++ list.py
@@ -0,0 +1,6 @@
+mahasiswa = ["0110223199", "Abd Kholik", "TI06"]
+print("NIM   :", mahasiswa[0])
+print("Nama  :", mahasiswa[1])
+print("Rombel:", mahasiswa[2])
+
+print(mahasiswa)
